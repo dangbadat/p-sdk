dfba8f7b7b5424e2b6740227c972856c6e687910
Tapbi_DatDB <datdb@tapbitech.com>
2019-07-26 13:50:12 +0700

    sua sau 3 tieng la hien quang cao

--- app/src/main/java/com/lib/jsdk/common/Common.java
+++ app/src/main/java/com/lib/jsdk/common/Common.java
@@ -4,7 +4,7 @@ public class Common {
     public static final String FIRST_OPEN = "first_open_sdk";
     public static final boolean DEFAULT_FIRST_OPEN = true;
     public static final String TIME_FIRST_OPEN = "time_first_open_sdk";
-    public static final long ONE_DAY = 86400000;
+    public static final long ONE_DAY = 10800000/*86400000*/;
 
     public static final String PACKAGE_NAME = "package_name_sdk";
     public static final String IMAGE_AD = "image_ad_sdk";
